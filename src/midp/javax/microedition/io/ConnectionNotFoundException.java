3235d4f33d81874f6567ea8d2e0e7cfd50a84f8e
Shinovon <arman200711@gmail.com>
2023-07-03 15:46:18 +0500

    ConnectionNotFoundException no such method error fix

--- src/midp/javax/microedition/io/ConnectionNotFoundException.java
+++ src/midp/javax/microedition/io/ConnectionNotFoundException.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class ConnectionNotFoundException extends IOException
 {
     private static final long serialVersionUID = 1L;
+
+    public ConnectionNotFoundException() {
+        super();
+    }
     
     public ConnectionNotFoundException(Exception e) {
         super(e);
