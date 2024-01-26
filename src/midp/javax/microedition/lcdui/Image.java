77d129837efc685878b3cd427d9a4f5e90b4d666
Shinovon <arman200711@gmail.com>
2024-01-26 19:50:55 +0500

    DirectUtils createImage x2

--- src/midp/javax/microedition/lcdui/Image.java
+++ src/midp/javax/microedition/lcdui/Image.java
@@ -116,13 +116,19 @@ public class Image
         final Image image;
         (image = new Image(Emulator.getEmulator().newImage(n, n2, true))).mutable = true;
         image.xrayBuffer = Emulator.getEmulator().newImage(n, n2, true);
-        if(image.getImpl() != null) {
-            IGraphics2D g = image.getImpl().getGraphics();
+        // мега костыль
+        try {
+            Graphics g = image.getGraphics();
             if (g != null) {
-                if ((color >> 24 & 0xFF) == 0) color |= 0xFF000000;
-                g.setColor(color, true);
+                int alpha = color >> 24 & 0xFF;
+                color |= 0xFF000000;
+                g.setColor(color);
+                g.getImpl().setAlpha(alpha);
                 g.fillRect(0, 0, n, n2);
+                g.getImpl().setAlpha(255);
+                g.setColor(0);
             }
+        } catch (Exception e) {
         }
         return image;
     }
