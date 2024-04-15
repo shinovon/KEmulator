6a9d302d38d8d8e66fe342732db0a7bf4821b7aa
Shinovon <arman200711@gmail.com>
2024-04-15 15:02:48 +0500

    Fix logs queue

--- src/main/emulator/ui/swt/Class11.java
+++ src/main/emulator/ui/swt/Class11.java
@@ -15,15 +15,15 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
     private Shell logShell;
     PrintStream filePrintStream;
     LogStream logStream;
-    private boolean logOpen;
-    private StyledText aStyledText574;
-    private Shell parentShell;
+    boolean logOpen;
+    StyledText styledText;
+    Shell parentShell;
     private boolean aBoolean576;
 
     public Class11() {
         super();
         this.logShell = null;
-        this.aStyledText574 = null;
+        this.styledText = null;
         try {
             final File file;
             if (!(file = new File(Emulator.getAbsolutePath() + "/log.txt")).exists()) {
@@ -35,63 +35,54 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
         }
     }
 
-    public void systemPrint(String s) {
-        this.filePrintStream.print(s);
-        if (this.logOpen) {
-            this.queuePrint(s);
-        }
-    }
-
-    public void systemPrintln(String s) {
-        this.filePrintStream.println(s);
-        if (this.logOpen) {
-            this.queuePrint(s + "\n");
-        }
-    }
-
+    // KEmulator api methods
     public final void print(final String s) {
         logStream.orig.print(s);
-        systemPrint(s);
+        queuePrint(s);
     }
 
     public final void println(final String s) {
         logStream.orig.println(s);
-        systemPrintln(s);
+        queuePrint(s + "\n");
     }
 
     public final void println() {
         logStream.orig.println();
-        this.filePrintStream.println();
-        if (this.logOpen) {
-            this.queuePrint("\n");
-        }
+        queuePrint("\n");
     }
 
     public final void stdout(final String s) {
-        this.filePrintStream.println(s);
-        this.logStream.systemPrint(s);
-        if (this.logOpen) {
-            this.queuePrint(s + "\n");
-        }
+        this.logStream.orig.println(s);
+        queuePrint(s + "\n");
     }
 
     public final void printStackTrace(final String s) {
         this.println("==StackTrace==" + s + "==StackTrace==");
     }
 
+    // System.out wrapper methods
+    public void wrapPrint(String s) {
+        queuePrint(s);
+    }
+
+    public void wrapPrintln(String s) {
+        queuePrint(s + "\n");
+    }
+
     private void queuePrint(final String s) {
         EmulatorImpl.asyncExec(new Textout(this, s));
     }
+
 
     private void method332() {
         try {
             final BufferedReader bufferedReader = new BufferedReader(new FileReader(Emulator.getAbsolutePath() + "/log.txt"));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                this.aStyledText574.append(line + "\n");
+                this.styledText.append(line + "\n");
             }
             bufferedReader.close();
-            this.aStyledText574.setTopIndex(this.aStyledText574.getLineCount());
+            this.styledText.setTopIndex(this.styledText.getLineCount());
         } catch (Exception ex) {
         }
     }
@@ -141,7 +132,7 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
         ((Decorations) this.logShell).setImage(new Image((Device) Display.getCurrent(), this.getClass().getResourceAsStream("/res/icon")));
         ((Composite) this.logShell).setLayout((Layout) layout);
         ((Control) this.logShell).setSize(new Point(310, 254));
-        this.aStyledText574 = new StyledText((Composite) this.logShell, 2816);
+        this.styledText = new StyledText((Composite) this.logShell, 2816);
     }
 
     public final boolean method333() {
@@ -149,17 +140,12 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
     }
 
     public final void controlMoved(final ControlEvent controlEvent) {
-        Class11 class11;
-        boolean aBoolean576;
         if (Math.abs(this.parentShell.getLocation().x + this.parentShell.getSize().x - this.logShell.getLocation().x) < 10 && Math.abs(this.parentShell.getLocation().y - this.logShell.getLocation().y) < 20) {
             ((Control) this.logShell).setLocation(this.parentShell.getLocation().x + this.parentShell.getSize().x, this.parentShell.getLocation().y);
-            class11 = this;
             aBoolean576 = true;
         } else {
-            class11 = this;
             aBoolean576 = false;
         }
-        class11.aBoolean576 = aBoolean576;
     }
 
     public final void controlResized(final ControlEvent controlEvent) {
@@ -169,26 +155,25 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
         this.method330();
     }
 
-    static StyledText method331(final Class11 class11) {
-        return class11.aStyledText574;
-    }
-
     private final class Textout implements Runnable {
-        private final Class11 aClass11_1301;
+        private final Class11 class11;
         private final String s;
 
         private Textout(final Class11 c11, String s) {
             super();
-            this.aClass11_1301 = c11;
+            this.class11 = c11;
             this.s = s;
         }
 
         public final void run() {
-            if (((Widget) Class11.method331(this.aClass11_1301)).isDisposed()) {
+            try {
+                class11.filePrintStream.print(s);
+            } catch (Exception e) {}
+            if (!class11.logOpen || class11.styledText.isDisposed() ) {
                 return;
             }
-            Class11.method331(this.aClass11_1301).append(s);
-            Class11.method331(this.aClass11_1301).setTopIndex(Class11.method331(this.aClass11_1301).getLineCount());
+            class11.styledText.append(s);
+            class11.styledText.setTopIndex(class11.styledText.getLineCount());
         }
     }
 
@@ -205,32 +190,28 @@ public final class Class11 implements ILogStream, ControlListener, DisposeListen
         }
 
         public final void print(final String s) {
-            this.class11.systemPrint(">>" + s);
+            this.class11.wrapPrint(">>" + s);
             this.orig.print(s);
         }
 
         public final void println(final String s) {
-            this.class11.systemPrintln(">>" + s);
+            this.class11.wrapPrintln(">>" + s);
             this.orig.println(s);
         }
 
         public final void print(final Object o) {
-            this.class11.systemPrint(">>" + o);
+            this.class11.wrapPrint(">>" + o);
             this.orig.print(o);
         }
 
         public final void println(final Object o) {
-            this.class11.systemPrintln(">>" + o);
+            this.class11.wrapPrintln(">>" + o);
             this.orig.println(o);
         }
 
         public final void println() {
-            this.class11.systemPrintln(">>");
+            this.class11.wrapPrintln(">>");
             this.orig.println();
-        }
-
-        public final void systemPrint(final String s) {
-            this.orig.println(s);
         }
     }
 }
