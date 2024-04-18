14de177a9d3f7b2d79c542b5175761a246077a57
rmn20 <romax9lahin@gmail.com>
2024-04-18 23:26:50 +0300

    m3_lwjgl Graphics3D.bindTarget refactor

--- m3g_lwjgl/src/javax/microedition/m3g/Graphics3D.java
+++ m3g_lwjgl/src/javax/microedition/m3g/Graphics3D.java
@@ -46,62 +46,71 @@ public class Graphics3D {
       this.bindTarget(var1, true, 0);
    }
 
-   public void bindTarget(Object var1, boolean var2, int var3) {
-      impl.enableDepthBuffer(var2);
-      if(target != null) {
+   public void bindTarget(Object target, boolean depthBuffer, int hints) {
+      impl.enableDepthBuffer(depthBuffer);
+
+      if(Graphics3D.target != null) {
          throw new IllegalStateException();
-      } else if(var1 == null) {
+      } else if(target == null) {
          throw new NullPointerException();
-      } else if(var3 != 0 && (var3 & 30) == 0) {
+      } else if(hints != 0 && (hints & (ANTIALIAS | DITHER | OVERWRITE | TRUE_COLOR)) == 0) {
          throw new IllegalArgumentException();
+      }
+
+      boolean overwrite = false;//(hints & OVERWRITE) != 0;
+
+      Background bck = new Background();
+      bck.setColorClearEnable(!overwrite);
+      bck.setDepthClearEnable(depthBuffer);
+
+      if(target instanceof Graphics) {
+         Graphics g = (Graphics) target;
+
+         int targetW = g.getImage().getWidth();
+         int targetH = g.getImage().getHeight();
+         if(targetW > Emulator3D.MaxViewportWidth || targetH > Emulator3D.MaxViewportHeight) {
+            throw new IllegalArgumentException();
+         }
+
+         Graphics3D.target = target;
+         impl.bindTarget(g);
+         setViewport(g.getClipX(), g.getClipY(), g.getClipWidth(), g.getClipHeight());
+
+         if(!overwrite) {
+            Image2D bckImage = new Image2D(Image2D.RGB, new Image(g.getImage()));
+            bck.setImage(bckImage);
+            bck.setCrop(this.viewportX, this.viewportY, this.viewportWidth, this.viewportHeight);
+         }
+      } else if (target instanceof Image2D) {
+         Image2D img2d = (Image2D) target;
+
+         int targetW = img2d.getWidth();
+         int targetH = img2d.getHeight();
+         if(targetW > Emulator3D.MaxViewportWidth || targetH > Emulator3D.MaxViewportHeight) {
+            throw new IllegalArgumentException();
+         }
+
+         int imageFormat = img2d.getFormat();
+         if(!img2d.isMutable() || imageFormat != Image2D.RGB && imageFormat != Image2D.RGBA) {
+            throw new IllegalArgumentException();
+         }
+
+         Graphics3D.target = target;
+         impl.bindTarget(img2d);
+         setViewport(0, 0, targetW, targetH);
+
+         if(!overwrite) {
+            bck.setImage(img2d);
+            bck.setCrop(viewportX, viewportY, viewportWidth, viewportHeight);
+         }
       } else {
-         Background var4 = new Background();
-         int var6;
-         int var7;
-         if(var1 instanceof Graphics) {
-            Graphics var5;
-            var6 = (var5 = (Graphics)var1).getImage().getWidth();
-            var7 = var5.getImage().getHeight();
-            if(var6 > Emulator3D.MaxViewportWidth || var7 > Emulator3D.MaxViewportHeight) {
-               throw new IllegalArgumentException();
-            }
+         throw new IllegalArgumentException();
+      }
 
-            target = var1;
-            impl.bindTarget(var5);
-            this.setViewport(var5.getClipX(), var5.getClipY(), var5.getClipWidth(), var5.getClipHeight());
-            Image2D var8 = new Image2D(99, new Image(var5.getImage()));
-            var4.setImage(var8);
-            var4.setCrop(this.viewportX, this.viewportY, this.viewportWidth, this.viewportHeight);
-         } else {
-            if(!(var1 instanceof Image2D)) {
-               throw new IllegalArgumentException();
-            }
-
-            Image2D var9;
-            var6 = (var9 = (Image2D)var1).getWidth();
-            var7 = var9.getHeight();
-            if(var9.getWidth() > Emulator3D.MaxViewportWidth || var9.getHeight() > Emulator3D.MaxViewportHeight) {
-               throw new IllegalArgumentException();
-            }
-
-            int var10 = var9.getFormat();
-            if(!var9.isMutable() || var10 != 99 && var10 != 100) {
-               throw new IllegalArgumentException();
-            }
-
-            target = var1;
-            impl.bindTarget(var9);
-            this.setViewport(0, 0, var6, var7);
-            var4.setImage(var9);
-            var4.setCrop(this.viewportX, this.viewportY, this.viewportWidth, this.viewportHeight);
-         }
-
-         if(target != null) {
-            impl.setHints(var3);
-            this.setDepthRange(this.depthRangeNear, this.depthRangeFar);
-            this.clear(var4);
-         }
-
+      if(Graphics3D.target != null) {
+         impl.setHints(hints);
+         setDepthRange(this.depthRangeNear, this.depthRangeFar);
+         clear(bck);
       }
    }
 
