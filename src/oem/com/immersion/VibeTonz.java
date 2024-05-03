e3eb122911dc4937321c23e5f98c7749727d6d73
Shinovon <arman200711@gmail.com>
2024-05-04 04:31:08 +0500

    Stub com.immersion.VibeTonz

---
+++ src/oem/com/immersion/VibeTonz.java
@@ -0,0 +1,12 @@
+package com.immersion;
+
+public class VibeTonz {
+
+    public int OpenDevice(int i) {
+        return 0;
+    }
+
+    public void SetDevicePropertyString(int n1, int n2, String s) {}
+
+    public void PlayPeriodicEffect(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9) {}
+}
