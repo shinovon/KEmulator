b562f3c19e07f67e00c6b5fbb93507ba7de1b4d2
Shinovon <arman200711@gmail.com>
2024-05-04 05:07:30 +0500

    Fix VibeTonz

--- src/oem/com/immersion/VibeTonz.java
+++ src/oem/com/immersion/VibeTonz.java
@@ -8,5 +8,7 @@ public class VibeTonz {
 
     public void SetDevicePropertyString(int n1, int n2, String s) {}
 
-    public void PlayPeriodicEffect(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9) {}
+    public int PlayPeriodicEffect(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9) {
+        return 0;
+    }
 }
