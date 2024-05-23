e9557122a19a76d0a585a5ec854687cf1c040fbc
Shinovon <arman200711@gmail.com>
2024-05-24 04:21:09 +0500

    Stub PantechAudio
    #30

---
+++ src/oem/com/pantech/titan/PantechAudio.java
@@ -0,0 +1,16 @@
+package com.pantech.titan;
+
+public class PantechAudio {
+
+    public PantechAudio(int n, String s) {
+
+    }
+
+    public void start(int n1, int n2) {
+
+    }
+
+    public void stop() {
+
+    }
+}
