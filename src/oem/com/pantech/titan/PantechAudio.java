db216589184aba86052b2f6a1868691a95dd7021
Shinovon <arman200711@gmail.com>
2024-05-24 04:30:21 +0500

    PantechAudio over Samsung API
    Close #30

--- src/oem/com/pantech/titan/PantechAudio.java
+++ src/oem/com/pantech/titan/PantechAudio.java
@@ -1,16 +1,26 @@
 package com.pantech.titan;
 
+import com.samsung.util.AudioClip;
+
 public class PantechAudio {
 
-    public PantechAudio(int n, String s) {
+    private AudioClip samsungImpl;
 
+    public PantechAudio(int n, String s) {
+        try {
+            samsungImpl = new AudioClip(AudioClip.TYPE_MMF, s);
+        } catch (Exception ignored) {}
     }
 
     public void start(int n1, int n2) {
-
+        try {
+            if(samsungImpl != null) samsungImpl.play(n1, 5);
+        } catch (Exception ignored) {}
     }
 
     public void stop() {
-
+        try {
+            if(samsungImpl != null) samsungImpl.stop();
+        } catch (Exception ignored) {}
     }
 }
