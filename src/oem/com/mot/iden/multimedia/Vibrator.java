e709f92f2239c217a82232b0555d948b4de517c7
Shinovon <arman200711@gmail.com>
2024-06-04 03:22:30 +0500

    com.mot.iden.multimedia.Vibrator

---
+++ src/oem/com/mot/iden/multimedia/Vibrator.java
@@ -0,0 +1,33 @@
+package com.mot.iden.multimedia;
+
+import emulator.Emulator;
+
+import java.util.TimerTask;
+
+public class Vibrator
+		extends Thread {
+	public static final int MAX_VIBRATE_TIME = 500;
+	public static final int MIN_PAUSE_TIME = 50;
+
+	public void run() {
+	}
+
+	public static void vibrateFor(int timeInMs) {
+		if (timeInMs > MAX_VIBRATE_TIME) timeInMs = MAX_VIBRATE_TIME;
+		Emulator.getEmulator().getScreen().startVibra(timeInMs);
+	}
+
+	public static void vibratePeriodically(int timeInMs) {
+	}
+
+	public static void vibratePeriodically(int timeOnInMs, int timeOffInMs) {
+	}
+
+	public static void vibratorOff() {
+		Emulator.getEmulator().getScreen().startVibra(0);
+	}
+
+	public static void vibratorOn() {
+		Emulator.getEmulator().getScreen().startVibra(3000);
+	}
+}
