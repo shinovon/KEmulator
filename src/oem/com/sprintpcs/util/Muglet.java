5368b28d1fa05adb242bbf8dd733dde12c6f77c3
Shinovon <arman200711@gmail.com>
2024-08-29 14:54:01 +0500

    Stub com.sprintpcs.util.Mugler

---
+++ src/oem/com/sprintpcs/util/Muglet.java
@@ -0,0 +1,21 @@
+package com.sprintpcs.util;
+
+public class Muglet {
+
+	public static Muglet getMuglet() {
+		return new Muglet();
+	}
+
+	public String getMediaType() {
+		return null;
+	}
+
+	public String getURI() {
+		return null;
+	}
+
+	public String getReferringURI() {
+		return null;
+	}
+
+}
