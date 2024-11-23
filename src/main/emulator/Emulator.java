6ecb5e6e76a6479a76c156570545d7575c746bf0
Shinovon <arman200711@gmail.com>
2024-11-23 21:45:24 +0500

    Bump version

--- src/main/emulator/Emulator.java
+++ src/main/emulator/Emulator.java
@@ -36,9 +36,9 @@ import emulator.ui.swt.EmulatorImpl;
 
 public class Emulator implements Runnable {
 	public static boolean debugBuild = true;
-	public static String version = "2.17.3";
+	public static String version = "2.17.4";
 	public static String revision = "";
-	public static final int numericVersion = 23;
+	public static final int numericVersion = 24;
 
 	static EmulatorImpl emulatorimpl;
 	private static MIDlet midlet;
