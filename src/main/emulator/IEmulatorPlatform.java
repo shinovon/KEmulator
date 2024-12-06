8808a119a55c0370fb6a91611d008936c8bc0b85
Shinovon <arman200711@gmail.com>
2024-12-06 11:43:44 +0500

    Remove redundant code

--- src/main/emulator/IEmulatorPlatform.java
+++ src/main/emulator/IEmulatorPlatform.java
@@ -23,20 +23,4 @@ public interface IEmulatorPlatform {
 	MemoryViewImage convertMicro3DTexture(Object o);
 
 	IGraphics3D getGraphics3D();
-
-	long createGLContext(long gcHandle, boolean b) throws Exception;
-
-	boolean isGLContextCurrent(long imgHandle) throws Exception;
-
-	void setGLContextCurrent(long gcHandle, long contextHandle) throws Exception;
-
-	void releaseGLContext(long gcHandle) throws Exception;
-
-	void deleteGLContext(long contextHandle) throws Exception;
-
-	long getDC(long handle) throws Exception;
-
-	void releaseDC(long handle, long dc) throws Exception;
-
-	void swapBuffers(long dc) throws Exception;
 }
--- win/src/main/emulator/EmulatorPlatform.java
+++ win/src/main/emulator/EmulatorPlatform.java
@@ -12,36 +12,6 @@ import java.nio.IntBuffer;
 
 public class EmulatorPlatform implements IEmulatorPlatform {
 
-	private static Class wglClass;
-	private static Class pixelFormatDescriptorClass;
-	private static Method ChoosePixelFormat;
-	private static Method SetPixelFormat;
-	private static Method wglCreateContext;
-	private static Method wglDeleteContext;
-	private static Method wglMakeCurrent;
-	private static Method wglGetCurrentContext;
-	private static Class osClass;
-	private static Method getDC;
-	private static Method releaseDC;
-	private static Method SwapBuffers;
-
-	static {
-		try {
-			wglClass = Class.forName("org.eclipse.swt.internal.opengl.win32.WGL");
-			pixelFormatDescriptorClass = Class.forName("org.eclipse.swt.internal.opengl.win32.PIXELFORMATDESCRIPTOR");
-			osClass = Class.forName("org.eclipse.swt.internal.win32.OS");
-			ChoosePixelFormat = ReflectUtil.getMethod(wglClass, "ChoosePixelFormat", int.class, pixelFormatDescriptorClass);
-			SetPixelFormat = ReflectUtil.getMethod(wglClass, "SetPixelFormat", int.class, int.class, pixelFormatDescriptorClass);
-			wglCreateContext = ReflectUtil.getMethod(wglClass, "wglCreateContext", int.class);
-			wglMakeCurrent = ReflectUtil.getMethod(wglClass, "wglMakeCurrent", int.class, int.class);
-			wglDeleteContext = ReflectUtil.getMethod(wglClass, "wglDeleteContext", int.class);
-			SwapBuffers = ReflectUtil.getMethod(wglClass, "wglSwapBuffers", int.class);
-			wglGetCurrentContext = ReflectUtil.getMethod(wglClass, "wglGetCurrentContext");
-			getDC = ReflectUtil.getMethod(osClass, "getDC", int.class);
-			releaseDC = ReflectUtil.getMethod(osClass, "releaseDC", int.class, int.class);
-		} catch (Throwable ignored) {}
-	}
-
 	public boolean isX64() {
 		return false;
 	}
@@ -73,7 +43,6 @@ public class EmulatorPlatform implements IEmulatorPlatform {
 	}
 
 	public void loadLibraries() {
-		System.setProperty("org.lwjgl.librarypath", Emulator.getAbsolutePath());
 	}
 
 	public boolean supportsMascotCapsule() {
@@ -112,56 +81,6 @@ public class EmulatorPlatform implements IEmulatorPlatform {
 
 	public IGraphics3D getGraphics3D() {
 		return emulator.graphics3D.lwjgl.Emulator3D.getInstance();
-	}
-
-	public long createGLContext(long gcHandle, boolean b) throws Exception {
-		int handle = (int) gcHandle;
-		PIXELFORMATDESCRIPTOR var4 = new PIXELFORMATDESCRIPTOR();
-		var4.nSize = 40;
-		var4.nVersion = 1;
-		var4.dwFlags = 37 + (b ? 20 : 0);
-		var4.iPixelType = 0;
-		var4.cColorBits = (byte) Emulator.getEmulator().getScreenDepth();
-		var4.iLayerType = 0;
-		int var5 = (Integer) ChoosePixelFormat.invoke(null, handle, var4);
-		if (var5 == 0) {
-			return 0;
-		}
-
-		if (!((Boolean) SetPixelFormat.invoke(null, handle, var5, var4))) {
-			return 0;
-		}
-
-		return (Integer) wglCreateContext.invoke(null, handle);
-	}
-
-	public boolean isGLContextCurrent(long imgHandle) throws Exception {
-		return (Integer) wglGetCurrentContext.invoke(null) == imgHandle;
-	}
-
-	public void setGLContextCurrent(long gcHandle, long contextHandle) throws Exception {
-		while ((Integer) wglGetCurrentContext.invoke(null) > 0) ;
-		wglMakeCurrent.invoke(null, (int) gcHandle, (int) contextHandle);
-	}
-
-	public void releaseGLContext(long gcHandle) throws Exception {
-		wglMakeCurrent.invoke(null, (int) gcHandle, -1);
-	}
-
-	public void deleteGLContext(long contextHandle) throws Exception {
-		wglDeleteContext.invoke(null, (int) contextHandle);
-	}
-
-	public long getDC(long handle) throws Exception {
-		return (int) getDC.invoke(null, (int) handle);
-	}
-
-	public void releaseDC(long handle, long dc) throws Exception {
-		releaseDC.invoke(null, (int) handle, (int) dc);
-	}
-
-	public void swapBuffers(long dc) throws Exception {
-		SwapBuffers.invoke(null, (int) dc);
 	}
 
 	public void load3D() {
