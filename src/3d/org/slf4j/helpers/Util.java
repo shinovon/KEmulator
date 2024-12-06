63ce71b8f191262901aa3849eae61e51ffc4946e
Shinovon <arman200711@gmail.com>
2024-12-06 11:44:54 +0500

    Return accidentraly deleted util class

---
+++ src/3d/org/slf4j/helpers/Util.java
@@ -0,0 +1,12 @@
+package org.slf4j.helpers;
+
+public class Util {
+	public static final void report(String msg, Throwable t) {
+		System.err.println(msg);
+		System.err.println("Reported exception:");
+		t.printStackTrace();
+	}
+
+	public static final void report(String msg) {
+	}
+}
