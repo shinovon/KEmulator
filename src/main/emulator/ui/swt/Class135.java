abe763f0a8ca3b83147ecaa6757d7da6f3907f81
Shinovon <arman200711@gmail.com>
2024-12-18 09:41:19 +0500

    Fix key map settings
    Closes #99

--- src/main/emulator/ui/swt/Class135.java
+++ src/main/emulator/ui/swt/Class135.java
@@ -1,22 +1,27 @@
 package emulator.ui.swt;
 
+import emulator.Emulator;
 import emulator.KeyMapping;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.widgets.Text;
 
 final class Class135 extends KeyAdapter {
-	private final Property aClass38_1294;
+	private Text text;
+	private int i;
 
-	Class135(final Property aClass38_1294) {
+	Class135(Text text, int i) {
 		super();
-		this.aClass38_1294 = aClass38_1294;
+		this.text = text;
+		this.i = i;
 	}
 
 	public final void keyPressed(final KeyEvent keyEvent) {
 		keyEvent.keyCode &= 0xFEFFFFFF;
-		if (Property.method376(this.aClass38_1294).getSelectionIndex() == 0 && KeyMapping.keyToString(String.valueOf(keyEvent.keyCode)) != null) {
-			Property.method380(this.aClass38_1294).setText(KeyMapping.keyToString(String.valueOf(keyEvent.keyCode)));
-			Property.method365()[1] = Property.method380(this.aClass38_1294).getText().trim();
+		if (((Property) Emulator.getEmulator().getProperty()).controllerCombo.getSelectionIndex() == 0 && KeyMapping.keyToString(String.valueOf(keyEvent.keyCode)) != null) {
+			text.setText(KeyMapping.keyToString(String.valueOf(keyEvent.keyCode)));
+			Property.method365()[i] = text.getText().trim();
 		}
+		keyEvent.doit = false;
 	}
 }
