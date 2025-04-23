f372478c49848abb4d0f2783c0b2eec66b924c30
Shinovon <arman200711@gmail.com>
2025-04-23 07:06:31 +0500

    Shorten kemulator.sh

--- home/kemulator.sh
+++ home/kemulator.sh
@@ -8,39 +8,17 @@ SELF=`realpath $0`
 SELF_DIR=`dirname $SELF`
 KEMULATOR_JAR="$SELF_DIR/KEmulator.jar"
 JAVA_VER=$($JAVA -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F '.' '{sub("^$", "0", $2); print $1$2}')
+ARGS="$XMX -Djna.nosys=true -Djava.library.path=$SELF_DIR -Dfile.encoding=UTF-8 -javaagent:$KEMULATOR_JAR -XX:+IgnoreUnrecognizedVMOptions -XstartOnFirstThread"
+
+if [ $JAVA_VER -ge 90 ]
+then
+  ARGS="$ARGS --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED"
+fi
 
 if [ -z $1 ]
 then
-  if [ $JAVA_VER -ge 90 ]
-  then
-    "$JAVA" -XX:+IgnoreUnrecognizedVMOptions -XstartOnFirstThread \
-    "-Djava.library.path=$SELF_DIR" "-javaagent:$KEMULATOR_JAR" \
-    --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED \
-    --add-opens java.base/java.lang.ref=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED \
-    --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED \
-	-Djna.nosys=true -Dfile.encoding=UTF-8 "$XMX" \
-	-jar "$KEMULATOR_JAR" -s
-  else
-    "$JAVA" -XX:+IgnoreUnrecognizedVMOptions -XstartOnFirstThread \
-    "-Djava.library.path=$SELF_DIR" "-javaagent:$KEMULATOR_JAR" \
-    -Djna.nosys=true -Dfile.encoding=UTF-8 "$XMX" \
-	-jar "$KEMULATOR_JAR" -s
-  fi
+  "$JAVA" $ARGS -jar "$KEMULATOR_JAR" -s
 else
   FULL_JAR=$(realpath "$1")
-  if [ $JAVA_VER -ge 90 ]
-  then
-    "$JAVA" -XX:+IgnoreUnrecognizedVMOptions -XstartOnFirstThread \
-    "-Djava.library.path=$SELF_DIR" "-javaagent:$KEMULATOR_JAR" \
-    --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED \
-    --add-opens java.base/java.lang.ref=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED \
-    --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED \
-	-Djna.nosys=true -Dfile.encoding=UTF-8 "$XMX" \
-	-jar "$KEMULATOR_JAR" -s -jar "$FULL_JAR"
-  else
-    "$JAVA" -XX:+IgnoreUnrecognizedVMOptions -XstartOnFirstThread \
-    "-Djava.library.path=$SELF_DIR" "-javaagent:$KEMULATOR_JAR" \
-    -Djna.nosys=true -Dfile.encoding=UTF-8 "$XMX" \
-    -jar "$KEMULATOR_JAR" -s -jar "$FULL_JAR"
-  fi
+  "$JAVA" $ARGS -jar "$KEMULATOR_JAR" -s -jar "$FULL_JAR"
 fi
