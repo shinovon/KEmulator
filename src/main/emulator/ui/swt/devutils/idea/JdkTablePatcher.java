5897f2b93225571abeaf16ec5af530c5ddb69c8a
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2025-05-11 00:22:45 +0300

    Omit XML version declaration

--- src/main/emulator/ui/swt/devutils/idea/JdkTablePatcher.java
+++ src/main/emulator/ui/swt/devutils/idea/JdkTablePatcher.java
@@ -93,6 +93,7 @@ public class JdkTablePatcher {
 		TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		Transformer transformer = transformerFactory.newTransformer();
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
 		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 		DOMSource source = new DOMSource(doc);
 		StreamResult result = new StreamResult(new File(path));
