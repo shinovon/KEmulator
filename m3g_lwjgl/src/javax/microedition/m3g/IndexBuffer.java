6cc4772c910b84f920c25b692582699730cc86d8
Shinovon <arman200711@gmail.com>
2025-07-30 01:36:52 +0500

    Fix TriangleStripArray.getIndices(int[]) from m3g 1.1 api
    Closes #201

--- m3g_lwjgl/src/javax/microedition/m3g/IndexBuffer.java
+++ m3g_lwjgl/src/javax/microedition/m3g/IndexBuffer.java
@@ -3,20 +3,7 @@ package javax.microedition.m3g;
 public abstract class IndexBuffer extends Object3D {
 	int[] indices;
 
-	public int getIndexCount() {
-		return this.indices != null ? this.indices.length : 0;
-	}
+	public abstract int getIndexCount();
 
-	public void getIndices(int[] var1) {
-		if (var1 == null) {
-			throw new NullPointerException();
-		} else if (var1.length < this.getIndexCount()) {
-			throw new IllegalArgumentException();
-		} else {
-			if (this.indices != null) {
-				System.arraycopy(this.indices, 0, var1, 0, this.indices.length);
-			}
-
-		}
-	}
+	public abstract void getIndices(int[] var1);
 }
