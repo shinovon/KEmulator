4e03d605527dc60405078ff28d85a2cbd9ea61c7
Shinovon <arman200711@gmail.com>
2025-08-03 01:07:49 +0500

    Revert spaces in starter bat fix

--- home/KEmulator_Console.bat
+++ home/KEmulator_Console.bat
@@ -4,8 +4,8 @@
 set dir=%~dp0
 set f=%1
 if defined f (
-java "-Djava.library.path=%dir%" -Xmx512M -jar "%dir%KEmulator.jar" -jar "%1"
+java -Djava.library.path=%dir% -Xmx512M -jar "%dir%KEmulator.jar" -jar "%1"
 ) else (
-java "-Djava.library.path=%dir%" -Xmx512M -jar "%dir%KEmulator.jar"
+java -Djava.library.path=%dir% -Xmx512M -jar "%dir%KEmulator.jar"
 )
 pause
