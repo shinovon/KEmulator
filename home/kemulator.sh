554c4d6f5c10f0f87d0081da9d5d102a79b8a114
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2025-08-16 23:16:22 +0300

    Use args building style from master

--- home/kemulator.sh
+++ home/kemulator.sh
@@ -32,39 +32,55 @@ KEM_JAR="$KEM_DIR/KEmulator.jar"
 
 # Hotspot configuration
 XMX="-Xmx512M"
-ARGS="$XMX -Djna.nosys=true -Dfile.encoding=UTF-8 -XX:+IgnoreUnrecognizedVMOptions"
+ARGS=(
+  "$XMX"
+  "-Djna.nosys=true"
+  "-Djava.library.path=$KEM_DIR"
+  "-Dfile.encoding=UTF-8"
+  "-javaagent:$KEMULATOR_JAR"
+  "-XX:+IgnoreUnrecognizedVMOptions"
+)
 if [[ "$OSTYPE" == "darwin"* ]]
 then
-	ARGS="$ARGS -XstartOnFirstThread"
+	 ARGS+=(
+    "-XstartOnFirstThread"
+  )
 fi
 if [ "$JAVA_VER" -ge 90 ]
 then
-	ARGS="$ARGS --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang.ref=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --enable-native-access=ALL-UNNAMED"
+  ARGS+=(
+    "--add-opens" "java.base/java.lang=ALL-UNNAMED"
+    "--add-opens" "java.base/java.lang.reflect=ALL-UNNAMED"
+    "--add-opens" "java.base/java.lang.ref=ALL-UNNAMED"
+    "--add-opens" "java.base/java.io=ALL-UNNAMED"
+    "--add-opens" "java.base/java.util=ALL-UNNAMED"
+    "--enable-native-access=ALL-UNNAMED"
+  )
 fi
 
 if [ -z "$1" ]; then
     # run without arguments
 	cd "$KEM_DIR" || exit
-	"$JAVA" $ARGS "-Djava.library.path=$KEM_DIR" "-javaagent:$KEM_JAR" -jar "$KEM_JAR" -s
+	"$JAVA" "${ARGS[@]}" -jar "$KEM_JAR" -s
 	exit $?
 elif [[ "$1" == "-new-project" || "$1" == "-restore" || "$1" == "-convert" || "$1" == "-edit" ]]; then
     # utils for IntelliJ IDEA
 	CALL_PWD="$PWD"
 	cd "$KEM_DIR" || exit
-	"$JAVA" $ARGS "-Djava.library.path=$KEM_DIR" "-javaagent:$KEM_JAR" -jar "$KEM_JAR" "$1" "$CALL_PWD" -s
+	"$JAVA" "${ARGS[@]}" -jar "$KEM_JAR" "$1" "$CALL_PWD" -s
 	exit $?
 elif [[ "$1" == -* ]]; then
 	# direct configuration (jad, classpath, etc)
 	echo "When using direct emulator configuration, please pass absolute paths!"
 	#TODO invent something to resolve jads/cp paths
 	cd "$KEM_DIR" || exit
-	"$JAVA" $ARGS "-Djava.library.path=$KEM_DIR" "-javaagent:$KEM_JAR" -jar "$KEM_JAR" -s "$@"
+	"$JAVA" "${ARGS[@]}" -jar "$KEM_JAR" -s "$@"
 	exit $?
 else
     # JAR file for launch
 	USER_JAR=$(realpath "$1")
 	cd "$KEM_DIR" || exit
 	shift
-	"$JAVA" $ARGS "-Djava.library.path=$KEM_DIR" "-javaagent:$KEM_JAR" -jar "$KEM_JAR" -jar "$USER_JAR" -s "$@"
+	"$JAVA" "${ARGS[@]}" -jar "$KEM_JAR" -jar "$USER_JAR" -s "$@"
 	exit $?
 fi
