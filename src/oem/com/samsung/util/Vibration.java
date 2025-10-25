155394b39e69edd13cfe7c9d79e7036507b6fdba
Shinovon <arman200711@gmail.com>
2025-10-25 08:21:54 +0500

    Update changelog

--- src/oem/com/samsung/util/Vibration.java
+++ src/oem/com/samsung/util/Vibration.java
@@ -3,6 +3,16 @@ package com.samsung.util;
 import emulator.Emulator;
 
 public class Vibration {
+	public static boolean isSupportedbyDevice() {
+		return false;
+	}
+
+	public static void startVibration(int paramInt1, int paramInt2) {
+	}
+
+	public static void stopVibration() {
+	}
+
 	public static boolean isSupported() {
 		return true;
 	}
