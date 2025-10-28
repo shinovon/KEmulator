f056d33be7df86279f59d4b11451d6b7d24fe12a
klaxons1 <59658354+klaxons1@users.noreply.github.com>
2025-10-28 17:57:10 +0300

    Minor sensor deobfuscation
    according to https://github.com/SymbianSource/oss.FCL.sf.app.JRT/blob/master/javaextensions/sensor/javasrc/javax/microedition/sensor/

--- src/midp/javax/microedition/sensor/LimitCondition.java
+++ src/midp/javax/microedition/sensor/LimitCondition.java
@@ -3,31 +3,31 @@ package javax.microedition.sensor;
 import emulator.sensor.ConditionHelpers;
 
 public class LimitCondition implements Condition {
-	private final double aDouble378;
-	private final String aString379;
+	private final double limit;
+	private final String operator;
 
-	public LimitCondition(final double aDouble378, final String aString379) {
+	public LimitCondition(final double limit, final String operator) {
 		super();
-		if (aString379 == null) {
+		if (operator == null) {
 			throw new NullPointerException();
 		}
-		if (!ConditionHelpers.checkOperator(aString379)) {
+		if (!ConditionHelpers.checkOperator(operator)) {
 			throw new IllegalArgumentException();
 		}
-		this.aDouble378 = aDouble378;
-		this.aString379 = aString379;
+		this.limit = limit;
+		this.operator = operator;
 	}
 
 	public final double getLimit() {
-		return this.aDouble378;
+		return this.limit;
 	}
 
 	public final String getOperator() {
-		return this.aString379;
+		return this.operator;
 	}
 
 	public boolean isMet(final double n) {
-		return ConditionHelpers.checkValue(this.aString379, this.aDouble378, n);
+		return ConditionHelpers.checkValue(this.operator, this.limit, n);
 	}
 
 	public boolean isMet(final Object o) {
--- src/midp/javax/microedition/sensor/RangeCondition.java
+++ src/midp/javax/microedition/sensor/RangeCondition.java
@@ -3,46 +3,46 @@ package javax.microedition.sensor;
 import emulator.sensor.ConditionHelpers;
 
 public class RangeCondition implements Condition {
-	private final double aDouble378;
-	private final double aDouble380;
-	private final String aString379;
-	private final String aString381;
+	private final double lowerLimit;
+	private final double upperLimit;
+	private final String lowerOp;
+	private final String upperOp;
 
-	public RangeCondition(final double aDouble378, final String aString379, final double aDouble379, final String aString380) {
+	public RangeCondition(final double lowerLimit, final String lowerOp, final double aDouble379, final String aString380) {
 		super();
-		if (!ConditionHelpers.checkLowerOperator(aString379) || !ConditionHelpers.checkUpperOperator(aString380)) {
+		if (!ConditionHelpers.checkLowerOperator(lowerOp) || !ConditionHelpers.checkUpperOperator(aString380)) {
 			throw new IllegalArgumentException();
 		}
-		if (aDouble378 > aDouble379) {
+		if (lowerLimit > aDouble379) {
 			throw new IllegalArgumentException();
 		}
-		if (aDouble378 == aDouble379 && (!"ge".equals(aString379) || !"le".equals(aString380))) {
+		if (lowerLimit == aDouble379 && (!"ge".equals(lowerOp) || !"le".equals(aString380))) {
 			throw new IllegalArgumentException();
 		}
-		this.aDouble378 = aDouble378;
-		this.aDouble380 = aDouble379;
-		this.aString379 = aString379;
-		this.aString381 = aString380;
+		this.lowerLimit = lowerLimit;
+		this.upperLimit = aDouble379;
+		this.lowerOp = lowerOp;
+		this.upperOp = aString380;
 	}
 
 	public final double getLowerLimit() {
-		return this.aDouble378;
+		return this.lowerLimit;
 	}
 
 	public final String getLowerOp() {
-		return this.aString379;
+		return this.lowerOp;
 	}
 
 	public final double getUpperLimit() {
-		return this.aDouble380;
+		return this.upperLimit;
 	}
 
 	public final String getUpperOp() {
-		return this.aString381;
+		return this.upperOp;
 	}
 
 	public boolean isMet(final double n) {
-		return ConditionHelpers.checkValue(this.aString379, this.aDouble378, n) && ConditionHelpers.checkValue(this.aString381, this.aDouble380, n);
+		return ConditionHelpers.checkValue(this.lowerOp, this.lowerLimit, n) && ConditionHelpers.checkValue(this.upperOp, this.upperLimit, n);
 	}
 
 	public boolean isMet(final Object o) {
