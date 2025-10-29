ee6c772a65dc09fcdf28a5485863fb7ae13d349d
klaxons1 <59658354+klaxons1@users.noreply.github.com>
2025-10-29 14:10:41 +0300

    woops

--- m3g_lwjgl/src/emulator/graphics3D/m3g/PeekInputStream.java
+++ m3g_lwjgl/src/emulator/graphics3D/m3g/PeekInputStream.java
@@ -4,18 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public final class PeekInputStream extends InputStream {
-	private int[] PeekBuffer;
+	private int[] peekBuffer;
 	private InputStream stream;
 	private int buffered;
 	private int counter;
 
 	public final int read() throws IOException {
 		if (this.counter < this.buffered) {
-			return this.PeekBuffer[this.counter++];
+			return this.peekBuffer[this.counter++];
 		} else {
 			int var1 = this.stream.read();
-			if (this.buffered < this.PeekBuffer.length) {
-				this.PeekBuffer[this.buffered] = var1;
+			if (this.buffered < this.peekBuffer.length) {
+				this.peekBuffer[this.buffered] = var1;
 				++this.buffered;
 			}
 
@@ -42,6 +42,6 @@ public final class PeekInputStream extends InputStream {
 
 	public PeekInputStream(InputStream var1, int var2) {
 		this.stream = var1;
-		this.PeekBuffer = new int[var2];
+		this.peekBuffer = new int[var2];
 	}
 }
