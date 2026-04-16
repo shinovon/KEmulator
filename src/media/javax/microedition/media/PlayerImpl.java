9bbe67443e8863735fff5ba9c4dffb9122b233fe
Shinovon <arman200711@gmail.com>
2026-04-16 15:08:59 +0500

    Fix midi volume

--- src/media/javax/microedition/media/PlayerImpl.java
+++ src/media/javax/microedition/media/PlayerImpl.java
@@ -799,6 +799,7 @@ public class PlayerImpl implements Player, Runnable, LineListener, MetaEventList
 								false);
 						b = false;
 					}
+					setLevel(level);
 					midiPlaying = true;
 					if (!stopped)
 						synchronized (playLock) {
