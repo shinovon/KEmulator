3c5f734a97946385a49839d542f057a5ae877df5
Shinovon <arman200711@gmail.com>
2026-07-28 06:24:12 +0500

    Some sprint media api deobfuscation

--- src/oem/com/sprintpcs/media/Player.java
+++ src/oem/com/sprintpcs/media/Player.java
@@ -5,98 +5,98 @@ import javax.microedition.media.MediaException;
 import javax.microedition.media.control.ToneControl;
 
 public class Player {
-	private static int anInt905 = 0;
-	private static int anInt912 = 0;
-	private static Object anObject906 = null;
-	private static int anInt915 = -2;
-	private static int anInt917 = -2;
-	private static int anInt918 = -2;
-	static javax.microedition.media.Player aPlayer907 = null;
-	static javax.microedition.media.Player aPlayer913 = null;
-	static javax.microedition.media.Player aPlayer916 = null;
+	static int clipState = 0;
+	static int toneState = 0;
+	static Object current = null;
+	static int foregroundClipLoopCount = -2;
+	static int toneLoopCount = -2;
+	static int backgroundClipLoopCount = -2;
+	static javax.microedition.media.Player foregroundClipPlayer = null;
+	static javax.microedition.media.Player backgroundClipPlayer = null;
+	static javax.microedition.media.Player tonePlayer = null;
 	private static ToneControl aToneControl908 = null;
-	static Clip aClip909 = null;
-	static DualTone aDualTone910 = null;
-	static Clip aClip914 = null;
-	private static PlayerListener aPlayerListener911 = null;
+	static Clip foregroundClip = null;
+	static DualTone tone = null;
+	static Clip backgroundClip = null;
+	static PlayerListener playerListener = null;
 
 	public Player() {
 	}
 
 	public static void addPlayerListener(PlayerListener playerListener) {
-		new Class55(playerListener);
+		new PlayerListenerImpl(playerListener);
 	}
 
 	public static void play(Clip clip, int n) throws IllegalArgumentException {
 		if (n < -1) {
 			throw new IllegalArgumentException("Repeat must be -1 or greater");
 		}
-		if (clip == null || clip.priority < anInt912) {
+		if (clip == null || clip.priority < toneState) {
 			return;
 		}
 		try {
-			switch (anInt905) {
+			switch (clipState) {
 				case 0: {
 					break;
 				}
 				case 1: {
-					if (aPlayer907 != null) {
-						aPlayer907.close();
-						aPlayer907 = null;
+					if (foregroundClipPlayer != null) {
+						foregroundClipPlayer.close();
+						foregroundClipPlayer = null;
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				case 6: {
-					if (aPlayer916 != null) {
-						aPlayer916.close();
-						aPlayer916 = null;
+					if (tonePlayer != null) {
+						tonePlayer.close();
+						tonePlayer = null;
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				case 4: {
-					if (aPlayer913 != null) {
-						aPlayer913.stop();
+					if (backgroundClipPlayer != null) {
+						backgroundClipPlayer.stop();
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				default: {
 					return;
 				}
 			}
-			aPlayer907 = clip.method112();
-			anInt915 = n != -1 ? n + 1 : -1;
-			aPlayer907.setLoopCount(anInt915);
-			Class55 class55 = new Class55(aPlayerListener911);
-			Class55.method366(class55, aPlayer907);
-			anInt912 = clip.priority;
-			aClip909 = clip;
-			anInt905 = 1;
-			anObject906 = clip;
+			foregroundClipPlayer = clip.method112();
+			foregroundClipLoopCount = n != -1 ? n + 1 : -1;
+			foregroundClipPlayer.setLoopCount(foregroundClipLoopCount);
+			PlayerListenerImpl class55 = new PlayerListenerImpl(playerListener);
+			class55.addPlayerListener(foregroundClipPlayer);
+			toneState = clip.priority;
+			foregroundClip = clip;
+			clipState = 1;
+			current = clip;
 			Vibrator.vibrate((int) clip.vibration);
-			aPlayer907.start();
+			foregroundClipPlayer.start();
 			return;
 		} catch (MediaException mediaException) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
 			System.out.println("Player.play() clip encountered a MediaException: " + mediaException.getMessage());
 			mediaException.printStackTrace();
-			anInt905 = 0;
-			anObject906 = null;
-			aClip909 = null;
-			anInt912 = 0;
+			clipState = 0;
+			current = null;
+			foregroundClip = null;
+			toneState = 0;
 			return;
 		} catch (Exception exception) {
-			anInt905 = 0;
-			anObject906 = null;
-			aClip909 = null;
-			anInt912 = 0;
+			clipState = 0;
+			current = null;
+			foregroundClip = null;
+			toneState = 0;
 			exception.printStackTrace();
 			return;
 		}
@@ -106,35 +106,35 @@ public class Player {
 		if (n < -1) {
 			throw new IllegalArgumentException("Repeat must be -1 or greater");
 		}
-		if (dualTone.anInt514 < anInt912) {
+		if (dualTone.anInt514 < toneState) {
 			return;
 		}
 		try {
-			switch (anInt905) {
+			switch (clipState) {
 				case 1: {
-					if (aPlayer907 != null) {
-						aPlayer907.close();
-						aPlayer907 = null;
+					if (foregroundClipPlayer != null) {
+						foregroundClipPlayer.close();
+						foregroundClipPlayer = null;
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				case 4: {
-					if (aPlayer913 != null) {
-						aPlayer913.stop();
+					if (backgroundClipPlayer != null) {
+						backgroundClipPlayer.stop();
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				case 6: {
-					if (aPlayer916 != null) {
-						aPlayer916.close();
-						aPlayer916 = null;
+					if (tonePlayer != null) {
+						tonePlayer.close();
+						tonePlayer = null;
 					}
-					if (aPlayerListener911 == null) break;
-					aPlayerListener911.playerUpdate(7, anObject906);
+					if (playerListener == null) break;
+					playerListener.playerUpdate(7, current);
 					break;
 				}
 				default: {
@@ -142,27 +142,27 @@ public class Player {
 				}
 				case 0:
 			}
-			if (aPlayer916 != null) {
-				aPlayer916.close();
-				aPlayer916 = null;
+			if (tonePlayer != null) {
+				tonePlayer.close();
+				tonePlayer = null;
 			}
-			aPlayer916 = Manager.createPlayer("device://tone");
-			aPlayer916.realize();
-			aToneControl908 = (ToneControl) aPlayer916.getControl("ToneControl");
-			anInt917 = n != -1 ? n + 1 : -1;
-			aPlayer916.setLoopCount(anInt917);
+			tonePlayer = Manager.createPlayer("device://tone");
+			tonePlayer.realize();
+			aToneControl908 = (ToneControl) tonePlayer.getControl("ToneControl");
+			toneLoopCount = n != -1 ? n + 1 : -1;
+			tonePlayer.setLoopCount(toneLoopCount);
 			aToneControl908.setSequence(dualTone.aByteArray513);
-			Class55 class55 = new Class55(aPlayerListener911);
-			Class55.method366(class55, aPlayer916);
-			anInt912 = dualTone.anInt514;
-			anObject906 = dualTone;
-			aDualTone910 = dualTone;
-			anInt905 = 6;
-			aPlayer916.start();
+			PlayerListenerImpl class55 = new PlayerListenerImpl(playerListener);
+			class55.addPlayerListener(tonePlayer);
+			toneState = dualTone.anInt514;
+			current = dualTone;
+			tone = dualTone;
+			clipState = 6;
+			tonePlayer.start();
 			return;
 		} catch (MediaException mediaException) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
 			mediaException.printStackTrace();
 			System.out.println("Player.play(DualTone) encountered an exception: " + mediaException.getMessage());
@@ -180,9 +180,9 @@ public class Player {
 		if (clip == null) {
 			return;
 		}
-		switch (anInt905) {
+		switch (clipState) {
 			case 0: {
-				anInt905 = 4;
+				clipState = 4;
 				break;
 			}
 			default: {
@@ -193,38 +193,38 @@ public class Player {
 			case 6:
 		}
 		try {
-			if (aPlayer913 != null) {
-				aPlayer913.close();
+			if (backgroundClipPlayer != null) {
+				backgroundClipPlayer.close();
 			}
-			aPlayer913 = clip.method112();
-			anInt918 = n != -1 ? n + 1 : -1;
-			aPlayer913.setLoopCount(anInt918);
-			Class55 class55 = new Class55(aPlayerListener911);
-			Class55.method366(class55, aPlayer913);
-			aClip914 = clip;
-			if (anInt905 != 1 && anInt905 != 6) {
-				anObject906 = aClip914;
-				aPlayer913.start();
+			backgroundClipPlayer = clip.method112();
+			backgroundClipLoopCount = n != -1 ? n + 1 : -1;
+			backgroundClipPlayer.setLoopCount(backgroundClipLoopCount);
+			PlayerListenerImpl class55 = new PlayerListenerImpl(playerListener);
+			class55.addPlayerListener(backgroundClipPlayer);
+			backgroundClip = clip;
+			if (clipState != 1 && clipState != 6) {
+				current = backgroundClip;
+				backgroundClipPlayer.start();
 			}
 			return;
 		} catch (MediaException mediaException) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
 			mediaException.printStackTrace();
 			System.out.println("playBackground() encountered a MediaException: " + mediaException.getMessage());
-			anInt905 = 0;
-			anObject906 = null;
-			aClip914 = null;
+			clipState = 0;
+			current = null;
+			backgroundClip = null;
 			return;
 		} catch (Exception exception) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
 			System.out.println("playBackground() encountered an IOException: " + exception.getMessage());
-			anInt905 = 0;
-			anObject906 = null;
-			aClip914 = null;
+			clipState = 0;
+			current = null;
+			backgroundClip = null;
 			exception.printStackTrace();
 			return;
 		}
@@ -234,34 +234,34 @@ public class Player {
 		block8:
 		{
 			try {
-				switch (anInt905) {
+				switch (clipState) {
 					case 1: {
-						if (aPlayer907.getState() != 400) break;
-						anInt905 = 2;
-						aPlayer907.stop();
+						if (foregroundClipPlayer.getState() != 400) break;
+						clipState = 2;
+						foregroundClipPlayer.stop();
 						break block8;
 					}
 					case 4: {
-						if (aPlayer913.getState() != 400) break;
-						anInt905 = 5;
-						aPlayer913.stop();
+						if (backgroundClipPlayer.getState() != 400) break;
+						clipState = 5;
+						backgroundClipPlayer.stop();
 					}
 				}
 				return;
 			} catch (MediaException mediaException) {
 				System.out.println("Player.pause encountered an exception: " + mediaException.getMessage());
 				mediaException.printStackTrace();
-				anInt905 = 0;
-				anObject906 = null;
-				aClip914 = null;
-				if (aPlayerListener911 != null) {
-					aPlayerListener911.playerUpdate(2, anObject906);
+				clipState = 0;
+				current = null;
+				backgroundClip = null;
+				if (playerListener != null) {
+					playerListener.playerUpdate(2, current);
 				}
 				return;
 			} catch (Exception exception) {
-				anInt905 = 0;
-				anObject906 = null;
-				aClip914 = null;
+				clipState = 0;
+				current = null;
+				backgroundClip = null;
 				exception.printStackTrace();
 			}
 		}
@@ -269,15 +269,15 @@ public class Player {
 
 	public static void resume() {
 		try {
-			switch (anInt905) {
+			switch (clipState) {
 				case 2: {
-					anInt905 = 1;
-					aPlayer907.start();
+					clipState = 1;
+					foregroundClipPlayer.start();
 					break;
 				}
 				case 5: {
-					anInt905 = 4;
-					aPlayer913.start();
+					clipState = 4;
+					backgroundClipPlayer.start();
 					break;
 				}
 				default: {
@@ -285,47 +285,47 @@ public class Player {
 				}
 			}
 		} catch (Exception exception) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
-			anInt905 = 0;
-			anObject906 = null;
-			aClip914 = null;
+			clipState = 0;
+			current = null;
+			backgroundClip = null;
 			exception.printStackTrace();
 		}
 	}
 
 	public static void stop() {
-		switch (anInt905) {
+		switch (clipState) {
 			case 1: {
-				if (aPlayer907 == null) break;
-				aPlayer907.close();
-				aPlayer907 = null;
+				if (foregroundClipPlayer == null) break;
+				foregroundClipPlayer.close();
+				foregroundClipPlayer = null;
 				break;
 			}
 			case 2: {
-				if (aPlayer907 == null) break;
-				aPlayer907.close();
-				aPlayer907 = null;
+				if (foregroundClipPlayer == null) break;
+				foregroundClipPlayer.close();
+				foregroundClipPlayer = null;
 				break;
 			}
 			case 4: {
-				if (aPlayer913 == null) break;
-				aPlayer913.close();
-				aPlayer913 = null;
+				if (backgroundClipPlayer == null) break;
+				backgroundClipPlayer.close();
+				backgroundClipPlayer = null;
 				break;
 			}
 			case 5: {
-				if (aPlayer913 == null) break;
-				aPlayer913.close();
-				aPlayer913 = null;
+				if (backgroundClipPlayer == null) break;
+				backgroundClipPlayer.close();
+				backgroundClipPlayer = null;
 				break;
 			}
 			case 6: {
-				if (aPlayer916 == null) break;
-				aPlayer916.close();
+				if (tonePlayer == null) break;
+				tonePlayer.close();
 				System.out.println("entering here");
-				aPlayer916 = null;
+				tonePlayer = null;
 				break;
 			}
 			default: {
@@ -333,60 +333,20 @@ public class Player {
 			}
 		}
 		try {
-			anInt905 = 0;
-			anObject906 = null;
-			anInt912 = 0;
+			clipState = 0;
+			current = null;
+			toneState = 0;
 			return;
 		} catch (Exception exception) {
-			if (aPlayerListener911 != null) {
-				aPlayerListener911.playerUpdate(2, anObject906);
+			if (playerListener != null) {
+				playerListener.playerUpdate(2, current);
 			}
-			anInt905 = 0;
-			anObject906 = null;
-			anInt912 = 0;
+			clipState = 0;
+			current = null;
+			toneState = 0;
 			exception.printStackTrace();
 			return;
 		}
-	}
-
-	static Object method313() {
-		return anObject906;
-	}
-
-	static PlayerListener method314() {
-		return aPlayerListener911;
-	}
-
-	static int method315() {
-		return anInt905;
-	}
-
-	static int method319() {
-		return anInt915;
-	}
-
-	static int method321() {
-		return anInt917;
-	}
-
-	static Object method316(Object object) {
-		anObject906 = object;
-		return anObject906;
-	}
-
-	static int method317(int n) {
-		anInt905 = n;
-		return anInt905;
-	}
-
-	static int method320(int n) {
-		anInt912 = n;
-		return anInt912;
-	}
-
-	static PlayerListener method318(PlayerListener playerListener) {
-		aPlayerListener911 = playerListener;
-		return aPlayerListener911;
 	}
 
 	static {
